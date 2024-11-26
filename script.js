14614237f6e914e03ed73cbbbbfc1f0c31d391b0
Adakole Naomie <adakolenaomi049@gmail.com>
2024-11-26 11:17:59 -0800

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const mainonecontainer = document.querySelectorAll(".mainonecontainer")
+
+const observer = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+mainonecontainer.forEach(mainonecontainer =>{
+    observer.observe(mainonecontainer)
+})

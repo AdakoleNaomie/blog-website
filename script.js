797f0d5f2322a0e498cd65d30cd40dc8b54a63c5
Adakole Naomie <adakolenaomi049@gmail.com>
2024-12-02 02:09:23 -0800

    commit

--- script.js
+++ script.js
@@ -54,3 +54,82 @@ const observer2 = new IntersectionObserver(entries => {
 boxcontainer.forEach(boxcontainer =>{
     observer.observe(boxcontainer)
 })
+
+
+const boxcontainer3 = document.querySelectorAll(".boxcontainer3")
+
+const observer6 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+boxcontainer3.forEach(boxcontainer3 =>{
+    observer.observe(boxcontainer3)
+})
+
+
+const boximg3 = document.querySelectorAll(".boximg3")
+
+const observer3 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+boximg3.forEach(boximg3 =>{
+    observer.observe(boximg3)
+})
+
+const boximg2 = document.querySelectorAll(".boximg2")
+
+const observer4 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+boximg2.forEach(boximg2 =>{
+    observer.observe(boximg2)
+})
+
+const boximg4 = document.querySelectorAll(".boximg4")
+
+const observer5 = new IntersectionObserver(entries => {
+    
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        // the berlow commented code is for if you don't want the effect to be repetitive
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
+        console.log(entries)
+    })
+}, {
+    // threshold: 1,
+    rootMargin:"-100px",
+})
+boximg4.forEach(boximg4=>{
+    observer.observe(boximg4)
+})
+
